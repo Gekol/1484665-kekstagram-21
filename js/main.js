28355f9ce153d8d498d2d818ce257261a0112453
Gekol <george.sokolovsky2001@gmail.com>
2020-11-14 17:49:43 +0200

    module 6 task 1 done

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
 'use strict';
 
 (function () {
-  const pictures = document.querySelectorAll(`.picture`);
-  for (let i = 0; i < window.pictureData.length; i++) {
-    pictures[i].addEventListener(`click`, function () {
-      window.bigPictureSetup(window.pictureData[i]);
-    });
-    pictures[i].addEventListener(`mousedown`, function (evt) {
-      if (evt.key === `Enter`) {
+  window.addEventListener(`load`, function () {
+    const pictures = document.querySelectorAll(`.picture`);
+    for (let i = 0; i < window.pictureData.length; i++) {
+      pictures[i].addEventListener(`click`, function () {
         window.bigPictureSetup(window.pictureData[i]);
-      }
+      });
+      pictures[i].addEventListener(`mousedown`, function (evt) {
+        if (evt.key === `Enter`) {
+          window.bigPictureSetup(window.pictureData[i]);
+        }
+      });
+    }
+    const hashtagInput = document.querySelector(`.text__hashtags`);
+    hashtagInput.addEventListener(`change`, function () {
+      window.checkHashTag(hashtagInput);
     });
-  }
-  const hashtagInput = document.querySelector(`.text__hashtags`);
-  hashtagInput.addEventListener(`change`, function () {
-    window.checkHashTag(hashtagInput);
+    const commentInput = document.querySelector(`.text__description`);
+    commentInput.addEventListener(`change`, function () {
+      window.checkComment(commentInput);
+    });
+    window.uploadForm.setupUploadFormShowing();
+    window.uploadForm.setupUploadFormHiding();
+    const imageUploadPreview = document.querySelector(`.img-upload__preview`);
+    window.newPictureProperties.setupSizeChanging(imageUploadPreview);
+    window.newPictureProperties.setupEffectChanging(imageUploadPreview);
+    window.newPictureProperties.setupEffectIntensityChanging(imageUploadPreview);
+    document.querySelector(`.social__comment-count`).classList.add(`hidden`);
+    document.querySelector(`.comments-loader`).classList.add(`hidden`);
   });
-  const commentInput = document.querySelector(`.text__description`);
-  commentInput.addEventListener(`change`, function () {
-    window.checkComment(commentInput);
-  });
-  window.uploadForm.setupUploadFormShowing();
-  window.uploadForm.setupUploadFormHiding();
-  const imageUploadPreview = document.querySelector(`.img-upload__preview`);
-  window.newPictureProperties.setupSizeChanging(imageUploadPreview);
-  window.newPictureProperties.setupEffectChanging(imageUploadPreview);
-  window.newPictureProperties.setupEffectIntensityChanging(imageUploadPreview);
-  document.querySelector(`.social__comment-count`).classList.add(`hidden`);
-  document.querySelector(`.comments-loader`).classList.add(`hidden`);
 })();
